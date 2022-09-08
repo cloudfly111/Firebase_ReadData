22cf929a912fd9977128b468ce9f01c2c7498773
Chan Shiang-Yun <cloudfly4326@gmail.com>
2022-09-08 09:10:29 +0800

    connect to Firebase and write data to Real Time Firebase.

--- app/src/main/java/com/example/firebase_readdata/MainActivity.java
+++ app/src/main/java/com/example/firebase_readdata/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.firebase_readdata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        Log.d("main","FireDatabase="+ database);
+//      FireDatabase=com.google.firebase.database.FirebaseDatabase@6c0dc1b
+
+        FirebaseApp getApp = database.getApp();
+        DatabaseReference dataRef = database.getReference();
+        Log.d("main","FireApp="+ getApp);
+//      FirebaseApp{name=[DEFAULT], options=FirebaseOptions{applicationId=1:
+
+        Log.d("main","DatabaseReference="+ dataRef);
+//      DatabaseReference=https://fir-project-4a35d-default-rtdb.firebaseio.com
+
+        String dataKey = dataRef.getKey();
+        Log.d("main","key=" + dataKey);//key=null
+
+        FirebaseDatabase data = dataRef.getDatabase();
+        Log.d("main","dataRef_database = "+data);
+//      dataRef_database = com.google.firebase.database.FirebaseDatabase@b1021b8
+
+        DatabaseReference dataParent = dataRef.getParent();
+        Log.d("main","dataParent="+dataParent);
+
+        DatabaseReference databaseRef = database.getReference("message");
+        Log.d("main","dataRef="+dataRef);
+//      dataRef=https://fir-project-4a35d-default-rtdb.firebaseio.com
+        databaseRef.setValue("Hello World!");
     }
 }
